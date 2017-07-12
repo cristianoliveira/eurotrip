f34c919f13308453f2dd7ca338d7c068052fcbee
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-07-12 21:02:40 +0200

    feature: Add http server

---
+++ bus_route_challenge/src/github.com/cristianoliveira/eurotrip/api/api.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/cristianoliveira/eurotrip/common"
+)
+
+func Serve(settings *common.Setting) {
+	address := "0.0.0.0:" + settings.Port
+
+	repo, err := LoadItineraries(settings.FilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Data source: " + settings.FilePath)
+	fmt.Println("")
+	fmt.Println("Running on: " + address)
+	fmt.Println("Endpoints:")
+	fmt.Println("/api/")
+	fmt.Println("/api/direct?dep_sid={}&arr_sid={}")
+
+	http.HandleFunc("/api/", HandleIndex)
+	http.HandleFunc("/api/direct", HandleBusRouteSearch(repo))
+
+	log.Fatal(http.ListenAndServe(address, nil))
+}
