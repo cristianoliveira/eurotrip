71c02cc09a2acab645e3557d18461573173db66a
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-07-14 09:29:13 +0200

    tests: Change error messages to a generic message

--- src/github.com/cristianoliveira/eurotrip/api/store_test.go
+++ src/github.com/cristianoliveira/eurotrip/api/store_test.go
@@ -8,21 +8,21 @@ import (
 func TestFindingAnUnion(t *testing.T) {
 	route := NewRoute([]string{"3", "1", "6", "5"})
 
-	t.Run("It has connection between 1 and 2", func(tt *testing.T) {
+	t.Run("It has connection between 3 and 6", func(tt *testing.T) {
 		if !route.Connected("3", "6") {
-			tt.Errorf("Union between 3 and 6 not found")
+			tt.Errorf("Union not found")
 		}
 	})
 
 	t.Run("It has connection between 5 and 3", func(tt *testing.T) {
 		if !route.Connected("5", "3") {
-			tt.Errorf("Union between 10 and 2 not found")
+			tt.Errorf("Union not found")
 		}
 	})
 
 	t.Run("It has no connection between 10 and 50", func(tt *testing.T) {
 		if route.Connected("1", "50") {
-			tt.Errorf("Union between 1, 50 found")
+			tt.Errorf("Union was found and doesn't exists")
 		}
 	})
 }
@@ -37,13 +37,13 @@ func TestLoadItinerary(t *testing.T) {
 
 	t.Run("It has connection between 114 & 142", func(tt *testing.T) {
 		if !itineraries.Connected("114", "142") {
-			tt.Errorf("Union between 114 & 142 not found")
+			tt.Errorf("Union not found")
 		}
 	})
 
 	t.Run("It has no connection between 3 & 6", func(tt *testing.T) {
 		if itineraries.Connected("3", "6") {
-			tt.Errorf("Union between 3 and 6 not found")
+			tt.Errorf("Union was found and doesn't exists")
 		}
 	})
 }
